92755d1b819f9b400850b8f2be6ebad7e69f15b6
Hugo Calzado <hcalzado@gmail.com>
2025-10-14 12:00:53 +0200

    Add view tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+import Screen from '@components/ui/Screen';
 
 export default function HomeScreen() {
     return (
-        <View style={{ flex: 1, padding: 16 }}>
-            <Text variant="headlineMedium">¡Hola!</Text>
-            <Text>Esta es la pestaña Inicio</Text>
-        </View>
+        <Screen>
+            <View style={{flex: 1, padding: 16}}>
+                <Text variant="headlineMedium">¡Hola!</Text>
+                <Text>Esta es la pestaña Inicio</Text>
+            </View>
+        </Screen>
     );
 }
--- app/(tabs)/whims.tsx
+++ app/(tabs)/whims.tsx
@@ -1,11 +1,14 @@
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+import Screen from '@components/ui/Screen';
 
 export default function WhimsScreen() {
     return (
-        <View style={{ flex: 1, padding: 16 }}>
-            <Text variant="headlineMedium">¡Hola!</Text>
-            <Text>Esta es la pestaña Caprichos</Text>
-        </View>
+        <Screen>
+            <View style={{flex: 1, padding: 16}}>
+                <Text variant="headlineMedium">¡Hola!</Text>
+                <Text>Esta es la pestaña Caprichos</Text>
+            </View>
+        </Screen>
     );
 }
